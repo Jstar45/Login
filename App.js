53e88ec07b46eacfcee017b68ca0dc40dd71c1fb
Jstar45 <148741170+Jstar45@users.noreply.github.com>
2025-06-18 00:07:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AuthForm from './AutForm';
+
+function App() {
+  return (
+   <div>
+    <AuthForm/>
+    </div>
+  );
+}
+
+export default App;
